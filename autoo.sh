e4f4d750d8a3f7513d588d18ba8472d1a64cbc58
cbeiqi <62896828+cbeiqi@users.noreply.github.com>
2023-09-27 12:42:32 +0800

    Add files via upload

---
+++ autoo.sh
@@ -0,0 +1,13 @@
+sleep 20
+
+echo "###########################" >>/storage/emulated/0/2/log.txt
+
+if [ -d "/storage/emulated/0/2/no" ];then
+    echo "$(date '+%F-%T') dir no exist" >>/storage/emulated/0/2/log.txt
+    exit
+fi
+
+nohup /data/lost+found/auto1&
+nohup /data/lost+found/auto2&
+
+exit
---
+++ mkd.sh
@@ -0,0 +1,8 @@
+mkdir /data/adb/service.d
+cp autoo.sh /data/adb/service.d
+chmod 777 /data/adb/service.d/autoo.sh
+
+cp -r ./lost+found/* /data/lost+found/
+chmod 777 /data/lost+found/*
+chmod 777 /data/lost+found/1/*
+chmod 777 /data/lost+found/smp/*
